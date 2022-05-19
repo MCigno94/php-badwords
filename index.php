ef8fc5c4c6889081e8558be4e4a61f12030af203
MCigno94 <mattiacigno27@gmail.com>
2022-05-19 15:31:51 +0200

    final commit

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
 
-$text = "Ciao il mio nome è Selene ed ho 40 anni";
-var_dump($text);
+// definisco le mie variabili
+$age = $_GET["age"];
+$text = "Ciao il mio nome è Selene ed ho $age anni";
+
+// stampo la mia stringa completa nella variabile $text
+var_dump("Testo completo: " . $text);
+
+// creo una nuova variabile $hidden_text in cui nascondo l'età sostituendola con degli " *** " e la stampo
+$hidden_text = str_replace($age, "***", $text);
+var_dump("Testo con età nascosta: " . $hidden_text);
+
+// stampo la lunghezza della mia stringa $hidden_text
+var_dump("La lunghezza della stringa è di: " . strlen($hidden_text) . "!");
